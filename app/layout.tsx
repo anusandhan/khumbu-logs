a1afae3c9141fb6a20849aa418e3adbcaa746bc1
anusandhan <panusandhan@gmail.com>
2024-01-24 21:53:33 +0545

    Minor Fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,36 +1,36 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
-import { MountainSnow } from 'lucide-react'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { MountainSnow } from "lucide-react";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Khumbu Logs',
-  description: 'An app that let`s you generate aggregated worklogs',
-}
+  title: "Khumbu Logs",
+  description: "An app that let`s you generate aggregated worklogs",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <nav className='grid grid-cols-1 space-2' >
-          <div className='flex items-center gap-1.5'>
-          <div className='flex w-6 h-6 p-1 bg-slate-200 rounded justify-center items-center'>
-            <MountainSnow size={16}/>
+        <nav className="grid grid-cols-1 space-2">
+          <div className="flex items-center gap-1.5">
+            <div className="flex w-6 h-6 p-1 bg-slate-200 rounded justify-center items-center">
+              <MountainSnow size={16} />
+            </div>
+            <h4>Khumbu</h4>
           </div>
-          <h4>Khumbu</h4>
-          </div>
-          <p className='text-slate-600'>
-          Select a project or an individual to see the worklogs & summary
+          <p className="text-slate-600">
+            Select a project or an individual to see the worklogs & summary
           </p>
         </nav>
         {children}
-        </body>
+      </body>
     </html>
-  )
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -4,56 +4,52 @@ import {
   SelectItem,
   SelectTrigger,
   SelectValue,
-} from "@/components/ui/select"
+} from "@/components/ui/select";
 
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 
-import { Label } from "@/components/ui/label"
+import { Label } from "@/components/ui/label";
 
 export default function Home() {
   return (
     <main>
       <div className="flex gap-4 flex-row items-end w-1/2">
         <div className="basis-1/3">
-        <Label htmlFor="project">Project</Label>
-              <Select>
-                <SelectTrigger id="project">
-                  <SelectValue placeholder="Select" />
-                </SelectTrigger>
-                <SelectContent position="popper">
-                  <SelectItem value="aire">AIRE</SelectItem>
-                  <SelectItem value="gena">GenA</SelectItem>
-                  <SelectItem value="growth">Growth</SelectItem>
-                  <SelectItem value="acquisitions">Acquistions</SelectItem>
-                </SelectContent>
-              </Select>
+          <Label htmlFor="project">Project</Label>
+          <Select>
+            <SelectTrigger id="project">
+              <SelectValue placeholder="Select" />
+            </SelectTrigger>
+            <SelectContent position="popper">
+              <SelectItem value="aire">AIRE</SelectItem>
+              <SelectItem value="gena">GenA</SelectItem>
+              <SelectItem value="growth">Growth</SelectItem>
+              <SelectItem value="acquisitions">Acquistions</SelectItem>
+            </SelectContent>
+          </Select>
         </div>
 
         <div className="basis-1/3">
-        <Label htmlFor="date">Date</Label>
-              <Select>
-                <SelectTrigger id="date">
-                  <SelectValue placeholder="Select" />
-                </SelectTrigger>
-                <SelectContent position="popper">
-                  <SelectItem value="next">This Week</SelectItem>
-                  <SelectItem value="sveltekit">Past Week</SelectItem>
-                  <SelectItem value="astro">This Month</SelectItem>
-                  <SelectItem value="nuxt">This Year</SelectItem>
-                </SelectContent>
-              </Select>
+          <Label htmlFor="date">Date</Label>
+          <Select>
+            <SelectTrigger id="date">
+              <SelectValue placeholder="Select" />
+            </SelectTrigger>
+            <SelectContent position="popper">
+              <SelectItem value="next">This Week</SelectItem>
+              <SelectItem value="sveltekit">Past Week</SelectItem>
+              <SelectItem value="astro">This Month</SelectItem>
+              <SelectItem value="nuxt">This Year</SelectItem>
+            </SelectContent>
+          </Select>
         </div>
 
         <div className="basis-1/3 flex items-end">
-          <Button>
-            View Worklogs
-          </Button>
-
+          <Button>View Worklogs</Button>
         </div>
-              
+      </div>
 
-              
-            </div>
+      <div className="grid grid-cols-3 gap-8"> </div>
     </main>
-  )
+  );
 }
