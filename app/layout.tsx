0b037d0d374a004a7e136dd84ebabfc894c67a97
anusandhan <panusandhan@gmail.com>
2024-01-24 19:41:10 +0545

    Initial layout with select and buttons

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { MountainSnow } from 'lucide-react'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +17,20 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <nav className='grid grid-cols-1 space-2' >
+          <div className='flex items-center gap-1.5'>
+          <div className='flex w-6 h-6 p-1 bg-slate-200 rounded justify-center items-center'>
+            <MountainSnow size={16}/>
+          </div>
+          <h4>Khumbu</h4>
+          </div>
+          <p className='text-slate-600'>
+          Select a project or an individual to see the worklogs & summary
+          </p>
+        </nav>
+        {children}
+        </body>
     </html>
   )
 }
