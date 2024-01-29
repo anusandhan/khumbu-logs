edcdc1e2b5ba64eeba5b1360e8ff6c718ca0641d
anusandhan <panusandhan@gmail.com>
2024-01-29 14:42:13 +0545

    📤 Filter while selecting a project

--- app/api/notion/route.ts
+++ app/api/notion/route.ts
@@ -1,19 +1,43 @@
 import { Client } from "@notionhq/client";
 
 const notion = new Client({
-  auth: "secret_Sxswd4GatC0tEShSQ2lNtsov0uWfGfV7maU751qA0ck",
+  auth: process.env.NOTION_API_KEY,
 });
 
-async function getNotionData() {
-  const databaseId: string = "2b62ec8e0c7c4d3b9fb2e3a08103fae1";
-  const response = await notion.databases.query({
+// async function getNotionData() {
+//   const databaseId: string = process.env.NOTION_DATABASE_ID!;
+//   const response = await notion.databases.query({
+//     database_id: databaseId,
+//     filter: Filter,
+//   });
+//   return response;
+// }
+
+async function getNotionData(project: string) {
+  const databaseId: string = process.env.NOTION_DATABASE_ID!;
+
+  let queryOptions: any = {
     database_id: databaseId,
-  });
+  };
+
+  if (project) {
+    queryOptions.filter = {
+      property: "Project",
+      select: {
+        equals: project,
+      },
+    };
+  }
+
+  const response = await notion.databases.query(queryOptions);
   return response;
 }
 
 export async function GET(request: Request) {
-  const data = await getNotionData();
+  const url = new URL(request.url);
+  const project = url.searchParams.get("project") || "";
+
+  const data = await getNotionData(project);
   return new Response(JSON.stringify(data), {
     headers: {
       "Content-Type": "application/json",
--- app/page.tsx
+++ app/page.tsx
@@ -18,6 +18,10 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 
+import { Checkbox } from "@/components/ui/checkbox";
+
+import { Badge } from "@/components/ui/badge";
+
 import { Button } from "@/components/ui/button";
 
 import { Label } from "@/components/ui/label";
@@ -69,20 +73,36 @@ interface LogItem {
   // Add other properties as needed
 }
 
-async function getData() {
-  const response = await fetch("/api/notion");
-  const data = await response.json();
-  return data;
-}
-
 export default function Home() {
   const [logsData, setLogsData] = useState<LogItem[]>([]);
+  const [selectedProject, setSelectedProject] = useState<string>("");
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    getData().then((data) => {
+    console.log("Current logsData:", logsData);
+  }, [logsData]);
+
+  async function fetchData(project: String) {
+    setIsLoading(true);
+    try {
+      const response = await fetch(`/api/notion?project=${project}`);
+      const data = await response.json();
       setLogsData(data.results || []);
-    });
-  }, []);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+    setIsLoading(false);
+  }
+
+  // Click handler for the button
+  const handleButtonClick = () => {
+    fetchData(selectedProject);
+  };
+
+  const handleProjectChange = (value: string) => {
+    console.log(value);
+    setSelectedProject(value);
+  };
 
   if (!logsData) {
     return <p>Loading...</p>;
@@ -93,15 +113,15 @@ export default function Home() {
       <div className="flex gap-4 flex-row items-end w-1/2">
         <div className="basis-1/3">
           <Label htmlFor="project">Project</Label>
-          <Select>
+          <Select onValueChange={handleProjectChange}>
             <SelectTrigger id="project">
               <SelectValue placeholder="Select" />
             </SelectTrigger>
             <SelectContent position="popper">
-              <SelectItem value="aire">AIRE</SelectItem>
-              <SelectItem value="gena">GenA</SelectItem>
-              <SelectItem value="growth">Growth</SelectItem>
-              <SelectItem value="acquisitions">Acquistions</SelectItem>
+              <SelectItem value="AIRE">AIRE</SelectItem>
+              <SelectItem value="GenA">GenA</SelectItem>
+              <SelectItem value="Growth">Growth</SelectItem>
+              <SelectItem value="Acquisitions">Acquistions</SelectItem>
             </SelectContent>
           </Select>
         </div>
@@ -122,31 +142,71 @@ export default function Home() {
         </div>
 
         <div className="basis-1/3 flex items-end">
-          <Button>View Worklogs</Button>
+          <Button onClick={handleButtonClick}>View Worklogs</Button>
         </div>
       </div>
 
       <div className="grid grid-cols-3 gap-8 mt-12">
-        {logsData.map((item: LogItem) => (
-          <Card key={item.id}>
-            <CardHeader>
-              <CardTitle>{item.properties["Person"].select.name}</CardTitle>
-              <CardDescription>
-                Date: {item.properties["Log Date"].date.start}
-              </CardDescription>
-            </CardHeader>
-            <CardContent>
-              <p>
-                {item.properties["Done This Week"].rich_text
-                  .map((text) => text.plain_text)
-                  .join(" ")}
-              </p>
-            </CardContent>
-            <CardFooter>
-              <p>Project: {item.properties["Project"].select.name}</p>
-            </CardFooter>
-          </Card>
-        ))}
+        {isLoading ? (
+          <p>Loading...</p>
+        ) : selectedProject === "" ? (
+          <p>Please select a project to view worklogs.</p>
+        ) : logsData.length === 0 ? (
+          <p>No worklogs available for the selected project.</p>
+        ) : (
+          logsData.map((item) => (
+            <Card key={item.id}>
+              <CardHeader>
+                <CardTitle>{item.properties["Person"].select.name}</CardTitle>
+                <CardDescription>
+                  {/* <div>
+                  <Badge>{item.properties["Project"].select.name}</Badge>
+                </div> */}
+                </CardDescription>
+              </CardHeader>
+              <CardContent className="grid grid-cols-1 gap-4">
+                <div className="grid grid-cols-1 text-slate-500">
+                  <p className="font-semibold">✅ Done this week</p>
+                  <p>
+                    {item.properties["Done This Week"].rich_text
+                      .map((text) => text.plain_text)
+                      .join(" ")}
+                  </p>
+                </div>
+                <div className="grid grid-cols-1 text-slate-500">
+                  <p className="font-semibold">📤 Todo next week</p>
+                  <p>
+                    {item.properties["To-Do Next Week"].rich_text
+                      .map((text) => text.plain_text)
+                      .join(" ")}
+                  </p>
+                </div>
+                <div className="grid grid-cols-1 text-slate-500">
+                  <p className="font-semibold">💥 Blockers</p>
+                  <p>
+                    {item.properties["Blockers"].rich_text
+                      .map((text) => text.plain_text)
+                      .join(" ")}
+                  </p>
+                </div>
+              </CardContent>
+              <CardFooter className="flex justify-between items-center">
+                <div className="flex items-center space-x-2">
+                  <Checkbox id="select-log" />
+                  <label
+                    htmlFor="select-log"
+                    className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                  >
+                    Select
+                  </label>
+                </div>
+                <p className="text-sm text-muted-foreground">
+                  {item.properties["Log Date"].date.start}
+                </p>
+              </CardFooter>
+            </Card>
+          ))
+        )}
       </div>
     </main>
   );
